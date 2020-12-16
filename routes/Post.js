45a081feca647fa878416956cb34402213a6e291
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-12-16 13:34:03 +0300

    new routes changed

--- routes/Post.js
+++ routes/Post.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const Post = require('../models/Post');
+const { default: validator } = require('validator');
+const { sendVerificationEmail } = require('../utils/account');
 
 
 router.get('/', async (req, res) => {
@@ -39,10 +41,39 @@ router.get('/:id', async (req, res) => {
 router.post('/add', async (req, res) => {
   const { email, username, password } = req.body
   
-  if (!email) {
+  const digit = /^(?=.*\d)/
+  const upperLetter = /^(?=.*[A-Z])/
 
+  if (digit.test(password)) {
+    return res
+      .status(400)
+      .send({ msg: "Please write a password that is containing a number."})
+  } else if (upperLetter.test(password)) {
+    return res  
+      .status(400)
+      .send({ msg: "Please include at least one uppercase letter in your password. "})
+  } else if (!validator.isEmail(email)) {
+    return res
+      .status(400)
+      .send({ "Please enter a valid email. "})
   }
-}
+
+  let salt = bcrypt.genSalt(10)
+  const hashPassword = bcrypt.hash(password, salt);
+
+  let newUser = {
+    username,
+    email,
+    hashPassword
+  }
+
+  const user = new User(newUser)
+  user.save()
+  sendVerificationEmail(user)
+
+  return res
+    .status(200)
+    .send({ msg: "User registration succesful. Please verify your email. "})
 })
 
 
